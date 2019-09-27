d2c2c4b04366105d8bc758a198f0c7e5aaa45992
Flame2018 <54338887+Flame2018@users.noreply.github.com>
2019-09-27 14:59:28 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MethodsHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.myfirst();
+
+            prog.myfirst2(400,80);
+            prog.myfirst2(400,30);
+            prog.myfirst2(400,50);
+
+            
+        }
+
+        public void myfirst()
+        {
+            Console.WriteLine("challenge accepted");
+        }
+        public void myfirst2(float param1f, float param2f)
+        {
+            var Average = param1f / param2f;
+            Console.WriteLine(Average);
+        }
+
+        public void myfirst3(int[] array)
+        {
+            foreach(int x in array)
+            {
+                if(x == 7)
+                {
+                    Console.WriteLine(7);
+                }else
+                {
+                    Console.WriteLine("Lucky You!");
+                }
+            }
+        }
+
+    }
+}
